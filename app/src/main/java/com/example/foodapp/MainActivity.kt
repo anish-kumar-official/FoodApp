a6becde85a651bcec3d0c883e5e8266a8a4e949c
Anish kumar <anish.bplofficial@gmail.com>
2021-12-12 20:03:17 +0530

    inflation done

--- app/src/main/java/com/example/foodapp/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -1,27 +1,42 @@
 package com.example.foodapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-menuInflater.inflate(R.menu.front_page,menu)
+        menuInflater.inflate(R.menu.front_page,menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
        when(item.itemId){
-           R.id.itemLogin -> Toast.makeText(this,"Login clicked",Toast.LENGTH_SHORT).show()
-           R.id.itemAboutUs -> Toast.makeText(this,"About us clicked",Toast.LENGTH_SHORT).show()
-           R.id.itemProfile -> Toast.makeText(this,"Profile clicked",Toast.LENGTH_SHORT).show()
+           R.id.itemLogin -> {
+               Intent(this,LoginActivity::class.java).also {
+                   startActivity(it)
+               }
+           }
+           R.id.itemAboutUs ->{
+               Intent(this,AboutMe::class.java).also {
+                   startActivity(it)
+               }
+           }
+           R.id.itemProfile -> {
+               Intent(this,Profile::class.java).also {
+                   startActivity(it)
+               }
+           }
        }
         return true
     }
